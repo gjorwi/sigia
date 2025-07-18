97831a7025d428414a98412775f3df5347e0d4fa
jorwi garcia <jgjg.0503@gmail.com>
2025-07-18 09:52:49 -0400

    configuration

--- app/administrador/configuracion/page.jsx
+++ app/administrador/configuracion/page.jsx
@@ -393,11 +393,11 @@ const ConfiguracionesPage = () => {
       
       <div className="mt-6">
         <div className="sm:hidden">
-          <label htmlFor="tabs" className="sr-only">Selecciona una pestaña</label>
+          <label htmlFor="tabs" className=" text-gray-500">Selecciona una pestaña</label>
           <select
             id="tabs"
             name="tabs"
-            className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            className="block w-full pl-3 pr-10 py-2 text-gray-700 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
             value={activeTab}
             onChange={(e) => setActiveTab(e.target.value)}
           >
